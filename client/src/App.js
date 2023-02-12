541926b92059edae8c2f73473a39647bb782a825
erickadikah <erickadikah2030@gmail.com>
2023-02-12 23:33:29 +0300

    landing page implementation

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./App.css";
+import LandingPage from "./components/LandingPage";
+// import Feed from "./componentsChat";
+// import Sidebar from "./components/Sidebar";
+
+function App() {
+    return (
+      <div className="app">
+        <LandingPage />
+        {/* <Chat /> */}
+        </div>
+    );
+  }
+  
+  export default App;
