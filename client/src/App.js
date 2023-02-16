a01e2bbda8f360c1677c19ee08af69a056487f3d
IamLam163 <olamidealiyu@gmail.com>
2023-02-16 11:58:08 +0100

    pushed the linked about us page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import LandingPage from "./components/LandingPage";
 import { Login } from './components/Login';
 import Register from './components/Register';
 import Home from './components/Home';
+import About from './components/About';
 
 
 // import Feed from "./componentsChat";
@@ -13,18 +14,19 @@ import Home from './components/Home';
 
 function App() {
     return (
-      <>
-      <BrowserRouter>
-      
-      <Routes>
-      <Route path="/"  element={<LandingPage />} />
-          <Route path="/Login"  element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/Home" element={<Home />} />
-          </Routes>
-        </BrowserRouter>
-      </>
+        <>
+            <BrowserRouter>
+
+                <Routes>
+                    <Route path="/" element={<LandingPage />} />
+                    <Route path="/Login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/Home" element={<Home />} />
+                    <Route path="/about" element={<About />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
-  }
-  
-  export default App;
+}
+
+export default App;
